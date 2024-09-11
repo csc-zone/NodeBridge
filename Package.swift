e9db9abeee18523a4484b31a26513be74683057c
csc-zone <csc-zone@gmail.com>
2024-09-11 17:08:53 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             ]),
     ],
     dependencies: [
-        .package(url: "https://github.com/csc-zone/nodejs-ios.git", .branch("main")),
+        .package(url: "https://github.com/csc-zone/nodejs-ios.git", .branch("test")),
     ],
     targets: [
         .target(
